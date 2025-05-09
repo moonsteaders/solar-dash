ce59dfaa61e5486638738347c00eada377461dc2
Daniel Ramer <danielramer@Mac.lan>
2025-05-08 21:47:06 -0500

    Fix Three.js imports

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,18 @@
-export default {
-  base: './',
-} 
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '/solar-dash/',
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+      },
+    },
+  },
+  optimizeDeps: {
+    include: ['three', '@tweenjs/tween.js']
+  }
+}) 
